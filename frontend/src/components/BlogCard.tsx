b4d803dbfe5c4fcea1f7e228c769944c49d12e03
Kavya Goyal <kavyagoel262000@gmail.com>
2024-04-06 14:29:17 +0530

    deploy vercel.json

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -45,7 +45,7 @@ export function Circle(){
     return <div className="h-1 w-1 rounded-full bg-slate-500 "> 
     </div> 
 }
-export function Avatar({ name, size ="small" }: { name: string , size: "small" | "big"}) {
+export function Avatar({ name, size ="small" }: { name: string , size?: "small" | "big"}) {
     return <div className= {`relative inline-flex items-center justify-center w-4 h-4 overflow-hidden bg-gray-600 rounded-full ${size === "small" ? "w-6 h-6" : "w-10 h-10" }`}>
     <span className={`${size === "small" ? "text-xs" : "text-md"} font-semibold text-white  dark:text-white`}>
      {name[0]}
