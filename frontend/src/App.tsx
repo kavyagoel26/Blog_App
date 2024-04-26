1608ed7e05c00cf26cdc47fdda00d08449e12bbf
Kavya Goyal <kavyagoel262000@gmail.com>
2024-04-26 12:52:32 +0530

    routing commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,29 @@
 
 
-import { BrowserRouter , Routes ,Route } from 'react-router-dom'
-import { Signup } from './pages/Signup'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Signin } from './pages/Signin'
-import { Blog } from './pages/Blog'
+import { Signup } from './pages/Signup'
 import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
+import { Blog } from './pages/Blog'
+
 // import './App.css'
-
-
 function App() {
   return(
     <div>
-      <BrowserRouter>
+    
+    <Router>
       <Routes>
+        <Route path="/" element={<Signup/>}></Route>
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/signin" element={<Signin/>}></Route>
         <Route path="/blog/:id" element={<Blog/>}></Route>
         <Route path="/blogs" element={<Blogs/>}></Route>
         <Route path="/publish" element={<Publish/>}></Route>
       </Routes>
-      </BrowserRouter>
+      </Router> 
+      </div>
 
-    </div>
   )
-
 }
-
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,40 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+// const appRouter = createBrowserRouter([{
+//   path:"/",
+//   element:<App/>,
+//   children:[
+//     {
+//       path:"/signin",
+//       element:<Signin/>,
+//     },{
+//       path:"/signup",
+//       element:<Signup/>,
+//     },{
+//       path:"/blogs",
+//       element:<Blogs/>,
+//     },{
+//       path:"/blog:id",
+//       element:<Blog/>,
+//     },{
+//       path:"/publish",
+//       element:<Publish/>
+//     }
+//   ],
+//   //errorElement:<Error/>
+// }    
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//       <RouterProvider router={appRouter} />
+//   </React.StrictMode>
+// );
+
